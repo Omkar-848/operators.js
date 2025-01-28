d703ad1a27eecb497e3d5ff5747fa9f50cd0a0ba
OMKAR CHAVAN <ovchavan3456@gmail.com>
2025-01-28 06:29:42 -0800

    Add files via upload

---
+++ op_1.js
@@ -0,0 +1,16 @@
+//Arithmetic operators 
+//comparison (Relational) operators
+//Bitwise operators
+//logical operators
+//Assignment operators
+//special operators
+
+// these are all arithmetic operators
+var a =10;
+var b =50;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+ 
---
+++ op_2.js
@@ -0,0 +1,32 @@
+// Equal to (==)
+//comparison (Relational) operators
+
+let a = 10;
+let b = "10";
+console.log(a === b);
+
+// Comparison operators examples all types
+
+// Equal to (==)
+console.log(5 == "5"); // true (compares value, not type)
+
+// Strict equal to (===)
+console.log(5 === "5"); // false (compares value and type)
+
+// Not equal to (!=)
+console.log(5 != "5"); // false (compares value, not type)
+
+// Strict not equal to (!==)
+console.log(5 !== "5"); // true (compares value and type)
+
+// Greater than (>)
+console.log(10 > 5); // true
+
+// Less than (<)
+console.log(10 < 5); // false
+
+// Greater than or equal to (>=)
+console.log(10 >= 10); // true
+
+// Less than or equal to (<=)
+console.log(5 <= 10); // true
